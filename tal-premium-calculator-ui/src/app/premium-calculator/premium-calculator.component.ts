242f2a930d3938f5fe6abd8464c7ef157fed9233
Kaushal de Silva <kaushal.desilva@dws.com.au>
2023-02-22 17:59:50 +1100

    Added services calls.

--- tal-premium-calculator-ui/src/app/premium-calculator/premium-calculator.component.ts
+++ tal-premium-calculator-ui/src/app/premium-calculator/premium-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OccupationResponse, OccupationService, PremiumService } from 'src/api';
 
 @Component({
   selector: 'app-premium-calculator',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./premium-calculator.component.css']
 })
 export class PremiumCalculatorComponent {
+  constructor(
+    private occupationService: OccupationService, 
+    private premiumService: PremiumService) {}
+
+    // input values
+    name = "";
+    dateOfBirth = new Date(new Date().getFullYear(), 0, 1);
+    sumInsured = 0;
+
+    occupations$ = this.occupationService.apiOccupationGetOccupationsGet();
+
+    onOccupationChange(event : Event) {
+      var occupationId = (event.target as HTMLSelectElement).value;
+      var premium = this.premiumService.apiPremiumGetPremiumGet(occupationId, this.sumInsured, this.dateOfBirth.toString()).subscribe((response) => {
+          console.log(response);
+      });
+      console.log(this.dateOfBirth.toString());
+    }
 
 }
