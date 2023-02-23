5bdda0b962a017f5f43701f4a14b9aad022726a7
Kaushal de Silva <kaushal.desilva@dws.com.au>
2023-02-23 11:25:27 +1100

    Added client side validation.

--- tal-premium-calculator-ui/src/app/premium-calculator/premium-calculator.component.ts
+++ tal-premium-calculator-ui/src/app/premium-calculator/premium-calculator.component.ts
@@ -11,19 +11,134 @@ export class PremiumCalculatorComponent {
     private occupationService: OccupationService, 
     private premiumService: PremiumService) {}
 
-    // input values
+    // input values    
     name = "";
     dateOfBirth = new Date(new Date().getFullYear(), 0, 1);
     sumInsured = 0;
+    occupationId = "";
 
-    occupations$ = this.occupationService.apiOccupationGetOccupationsGet();
+    // output values
+    deathPremium = 0;
+    tpdMonthlyPremium = 0;
+
+    // validation values
+    // true by default, so alerts are not shown by default
+    nameValid = true;
+    dateOfBirthValid = true;
+    sumInsuredValid = true;
+    occupationIdValid = true;
+    // false by default, to invalid premium is not shown
+    premiumValid = false;
+
+    occupations = [] as OccupationResponse[];
+    _ = this.occupationService.apiOccupationGetOccupationsGet().subscribe(
+      {
+        next: (occupationsResponse) => {
+          // set occupations
+          this.occupations = occupationsResponse;
+          // set current occupation id to first item in list
+          this.occupationId = occupationsResponse[0].id ?? ""
+        }
+      }
+    );
 
     onOccupationChange(event : Event) {
-      var occupationId = (event.target as HTMLSelectElement).value;
-      var premium = this.premiumService.apiPremiumGetPremiumGet(occupationId, this.sumInsured, this.dateOfBirth.toString()).subscribe((response) => {
-          console.log(response);
-      });
-      console.log(this.dateOfBirth.toString());
+      // set occupation id
+      this.occupationId = (event.target as HTMLSelectElement).value;
+      // calculate premium
+      this.calculatePremium();
     }
 
+    calculatePremium() {
+      // validate input first
+      if (this.validateInput())
+      {
+        console.log("valid");
+        // call premium service
+        this.premiumService.apiPremiumGetPremiumGet(this.occupationId, this.sumInsured, this.dateOfBirth.toString()).subscribe(
+          {
+              next: (premiumResponse) => {
+                // set death premium/tpd monthly premium
+                this.deathPremium = premiumResponse.deathPremium ?? 0;
+                this.tpdMonthlyPremium = premiumResponse.tpdPremiumMonthly ?? 0
+                this.premiumValid = true;
+              },
+              error: (error) => {
+                this.deathPremium = 0;
+                this.tpdMonthlyPremium = 0;
+                this.premiumValid = false;
+                console.log(error);
+              },
+              complete: () => {} 
+          });
+      }
+    }
+
+    validateInput() : boolean
+    {
+      console.log(this.name);
+      console.log(this.dateOfBirth);
+      console.log(this.sumInsured);
+      console.log(this.occupationId);
+
+      // name cannot be null, undefined, empty or whitespace
+      if (this.name != null && this.name.trim() !== '') {
+        this.nameValid = true;
+      }
+      else
+      {
+        this.nameValid = false;
+      }
+
+      // date of birth must be set, and calculated age must be greater than or equal to 70
+      if (this.dateOfBirth != null && this.calculateAge(this.dateOfBirth) <= 70)
+      {
+        this.dateOfBirthValid = true;
+      }
+      else
+      {
+        this.dateOfBirthValid = false;
+      }
+
+      // sum insured must be set, and greater than 0
+      if (this.sumInsured != null && this.sumInsured > 0)
+      {
+        this.sumInsuredValid = true;
+      }
+      else
+      {
+        this.sumInsuredValid = false;
+      }
+
+      // occupationId cannot be null, undefined, empty or whitespace
+      if (this.occupationId != null && this.occupationId.trim() !== ''){
+        this.occupationIdValid = true;
+      }
+      else
+      {
+        this.occupationIdValid = false;
+      }
+
+      // everything must be valid to return true
+      return this.nameValid && this.dateOfBirthValid && this.sumInsuredValid && this.occupationIdValid;
+    }
+
+    calculateAge(dateOfBirth : Date)
+    {
+      // calculate age
+      var dob = new Date(dateOfBirth);
+      dob = new Date(dob.toDateString()); // removing time component
+      var now = new Date();
+      now = new Date(now.toDateString()); // removing time component
+      var age = now.getUTCFullYear() - dob.getUTCFullYear();
+      // handle leap years
+      var leapCheck = new Date(now);
+      leapCheck.setUTCFullYear(now.getUTCFullYear()-age);
+      if (dob > leapCheck)
+      {
+        age--;
+      }
+      
+      return age;
+    }
 }
